cee544cf629dd3cb96d8c8314bcf4438b7e9be58
Mike Kelly <mikekelly321@gmail.com>
2013-06-15 11:25:14 +0100

    first pass at basic HandPair#winner behaviour

--- lib/poker/hand_pair.rb
+++ lib/poker/hand_pair.rb
@@ -1,13 +1,32 @@
 module Poker
   class HandPair
-    attr_reader :hands_data
+    attr_reader :hands_data, :hand_factory
 
-    def initialize(hands)
+    def initialize(hands, opts={})
       @hands_data = hands
+      @hand_factory = opts[:hand_factory] || Hand.public_method(:new)
     end
 
     def to_s
       hands_data.map { |hand| hand.join ' ' }.join ' vs '
+    end
+
+    def winner
+      first_hand > second_hand ? first_hand : second_hand
+    end
+
+    private
+
+    def hands
+      @hands ||= hands_data.map { |hand_data| hand_factory.call hand_data }
+    end
+
+    def first_hand
+      hands.first
+    end
+
+    def second_hand
+      hands[1]
     end
   end
 end
--- spec/lib/poker/hand_pair_spec.rb
+++ spec/lib/poker/hand_pair_spec.rb
@@ -4,8 +4,26 @@ describe Poker::HandPair do
   describe "#to_s" do
     it "should join up the hand data 'first_hand vs second_hand'" do
       hands = [['1','2','3','4','5'], ['1','2','3','4','5']]
-      hand_pair = Poker::HandPair.new hands
+      hand_pair = Poker::HandPair.new hands, hand_factory: stub
       hand_pair.to_s.should == '1 2 3 4 5 vs 1 2 3 4 5'
+    end
+  end
+
+  describe "#winner" do
+    it "returns the first hand if its greater than the second" do
+      first_hand = mock(:first_hand)
+      hand_factory = mock(:hand_factory)
+
+      second_hand = stub
+      hand_data_stubs = [stub, stub]
+
+      hand_factory.should_receive(:call).with(hand_data_stubs[0]).and_return(first_hand)
+      hand_factory.should_receive(:call).with(hand_data_stubs[1]).and_return(second_hand)
+
+      first_hand.should_receive(:>).with(second_hand).and_return(true)
+
+      pair = Poker::HandPair.new(hand_data_stubs, hand_factory: hand_factory)
+      pair.winner.should == first_hand
     end
   end
 end
